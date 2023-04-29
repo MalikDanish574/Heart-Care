bdbd87e1d7739c0baa051d88e66d8def452640c1
MalikDanish574 <115067551+MalikDanish574@users.noreply.github.com>
2023-04-29 16:41:17 +0500

    done

--- lib/screens/choose.dart
+++ lib/screens/choose.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,70 +14,140 @@ import '../utils/images.dart';
 import 'AdminScreens/admindashboard.dart';
 import 'DoctorScreens/doctordashboard.dart';
 
-class Choose extends StatelessWidget {
+class Choose extends StatefulWidget {
+  @override
+  State<Choose> createState() => _ChooseState();
+}
+
+class _ChooseState extends State<Choose> {
+  final admin = FirebaseFirestore.instance.collection('Admin').snapshots();
+
+  final doctor = FirebaseFirestore.instance.collection('Doctor').snapshots();
+
+  final patient = FirebaseFirestore.instance.collection('Patient').snapshots();
+
+  bool isAdmin = false;
+
+  bool isPatient = false;
+
+  bool isDoctor = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.95,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(background), fit: BoxFit.cover)),
+          child: Container(
+        height: MediaQuery.of(context).size.height * 0.95,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(background), fit: BoxFit.cover)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 30.w, top: 50.h),
+              child: Image.asset(
+                logo,
+                height: 150,
+                width: 150,
+              ),
+            ),
+            SizedBox(
+              height: 170.h,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Text("Login As",
+                  style: TextStyle(
+                      color: textred,
+                      fontSize: 24,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w700)),
+            ),
+            SizedBox(
+              height: 90.h,
+            ),
+            StreamBuilder<QuerySnapshot>(
+                stream: doctor,
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  return Align(
+                      alignment: Alignment.center,
+                      child: button(
+                        title: 'Doctor',
+                        ontap: () {
+                          for (int i = 0; i < snapshot.data!.docs.length; i++) {
+                            if (snapshot.data!.docs[i]["email"] ==
+                                FirebaseAuth.instance.currentUser!.email) {
+                              isDoctor = true;
+                              isAdmin = false;
+                              isPatient = false;
+                              setState(() {});
+                            }
+                          }
 
-                  child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding:  EdgeInsets.only(left: 30.w,top: 50.h),
-                        child: Image.asset(logo,height: 150,width: 150,),
-                      ),
-                     
-                        SizedBox(height: 170.h,),
-                       Align(
-                        alignment: Alignment.center,
-                         child: Text("Login As",
-                         
-                         style: TextStyle(
-                           color: textred,
-                           fontSize: 24,
-                           fontFamily: 'Montserrat',
-                                             fontWeight: FontWeight.w700)
-                         ),
-                       ),
-                       SizedBox(height: 90.h,),
+                          isDoctor
+                              ? Get.to(() => DoctorDashboard())
+                              : Get.to(() => DoctorLogin());
+                        },
+                      ));
+                }),
+            SizedBox(
+              height: 30.h,
+            ),
+            StreamBuilder<QuerySnapshot>(
+                stream: patient,
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  return Align(
+                      alignment: Alignment.center,
+                      child: button(
+                        title: 'Patient',
+                        ontap: () {
+                          for (int i = 0; i < snapshot.data!.docs.length; i++) {
+                            if (snapshot.data!.docs[i]["email"] ==
+                                FirebaseAuth.instance.currentUser!.email) {
+                              isDoctor = false;
+                              isAdmin = false;
+                              isPatient = true;
+                              setState(() {});
+                            }
+                          }
 
-                       Align(
-                        alignment: Alignment.center,
-                        child: button(title: 'Doctor',ontap: (){
-                           if (FirebaseAuth.instance.currentUser!=null){
-                                Get.to(()=>DoctorDashboard());
-                              }else{
-                                Get.to(()=>DoctorLogin());
-                              }
-                        },)),
-                        SizedBox(height: 30.h,),
-                      Align(
-                        alignment: Alignment.center,
-                        child: button(title: 'Patient',ontap: (){
-                          if (FirebaseAuth.instance.currentUser!=null){
-                                Get.to(()=>PatientDashboard());
-                              }else{
-                                Get.to(()=>PatientLogin());
-                              }
-                        },)),
-                        SizedBox(height: 30.h,),
-                       Align(
-                        alignment: Alignment.center,
-                        child: button(title: 'Admin',ontap: (){
-                          if (FirebaseAuth.instance.currentUser!=null){
-                                Get.to(()=>AdminDashboard());
-                              }else{
-                                Get.to(()=>AdminLogin());
-                              }
-                        },))
-                    ],
-                  ),
+                          isPatient
+                              ? Get.to(() => PatientDashboard())
+                              : Get.to(() => PatientLogin());
+                        },
+                      ));
+                }),
+            SizedBox(
+              height: 30.h,
+            ),
+            StreamBuilder<QuerySnapshot>(
+                stream: admin,
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  return Align(
+                      alignment: Alignment.center,
+                      child: button(
+                        title: 'Admin',
+                        ontap: () {
+                          for (int i = 0; i < snapshot.data!.docs.length; i++) {
+                            if (snapshot.data!.docs[i]["email"] ==
+                                FirebaseAuth.instance.currentUser!.email) {
+                              isDoctor = false;
+                              isAdmin = true;
+                              isPatient = false;
+                              setState(() {});
+                            }
+
+                            isAdmin
+                                ? Get.to(() => AdminDashboard())
+                                : Get.to(() => AdminLogin());
+                          }
+                        },
+                      ));
+                })
+          ],
+        ),
       )),
     );
   }
